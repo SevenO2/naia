37915e7a012fd1a9caeabc2d859265bf81462187
SevenO2 <7.o.them@gmail.com>
2023-02-07 13:18:43 -0800

    found link conditioner

--- demos/bevy/shared/src/shared.rs
+++ demos/bevy/shared/src/shared.rs
@@ -8,8 +8,8 @@ pub fn shared_config() -> SharedConfig<Channels> {
     // Set tick rate to ~60 FPS
     let tick_interval = Some(Duration::from_millis(20));
 
-    //  let link_condition = None;
-    let link_condition = Some(LinkConditionerConfig::average_condition());
+     // let link_condition = None;
+    let link_condition = Some(LinkConditionerConfig::good_condition());
     //  let link_condition = Some(LinkConditionerConfig {
     //      incoming_latency: 500,
     //      incoming_jitter: 1,
